4e45ef5d5f4770b7b430a17c99728b91880ec89a
Noah Rosenfield <nrosenfi@gmail.com>
2025-10-27 22:31:12 -0400

    restyles interface

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,15 @@ import { LayerPanel } from "./components/Timeline/LayerPanel";
 function App() {
   return (
     <div className="app-container h-screen flex flex-col bg-gray-800">
-      <div className="flex-1 flex overflow-hidden">
-        {/* Left Sidebar - Media Library and Layer Panel */}
-        <div className="w-80 border-r border-gray-700 flex flex-col">
-          {/* Media Library - top half */}
-          <div className="h-1/2 border-b border-gray-700">
-            <MediaLibrary />
-          </div>
-          
-          {/* Layer Panel - bottom half */}
-          <div className="h-1/2">
-            <LayerPanel />
-          </div>
+      {/* Top Row - Media Library and Preview Panel */}
+      <div className="flex-1 flex flex-row overflow-hidden" style={{ display: 'flex', minHeight: 0 }}>
+        {/* Media Library - Left */}
+        <div id="media-library-panel" className="border-r border-gray-700" style={{ width: '320px', minWidth: '320px', maxWidth: '320px' }}>
+          <MediaLibrary />
         </div>
         
-        {/* Main Content Area - Preview and Timeline */}
-        <div className="flex-1 flex flex-col relative">
+        {/* Preview Panel - Right */}
+        <div id="preview-panel" className="flex-1 flex flex-col relative" style={{ minWidth: 0 }}>
           {/* Preview Panel Label */}
           <div className="absolute top-0 right-0 z-10 p-4">
             <h2 className="text-lg font-bold text-red-500 writing-mode-vertical transform rotate-0" style={{ writingMode: 'vertical-rl' }}>
@@ -34,15 +27,23 @@ function App() {
           <div className="flex-1 bg-black overflow-hidden">
             <PreviewPlayer />
           </div>
-          
-          {/* Timeline Panel */}
-          <div className="bg-gray-800 border-t border-gray-700">
-            <div className="p-2 border-b border-gray-700">
-              <h2 className="text-lg font-bold text-red-500 text-center">TIMELINE PANEL</h2>
-            </div>
-            <div className="h-56">
-              <Timeline />
-            </div>
+        </div>
+      </div>
+      
+      {/* Bottom Row - Layer Panel and Timeline Panel */}
+      <div className="h-64 flex flex-row border-t border-gray-700" style={{ display: 'flex' }}>
+        {/* Layer Panel - Left */}
+        <div id="layer-panel" className="border-r border-gray-700" style={{ width: '320px', minWidth: '320px', maxWidth: '320px' }}>
+          <LayerPanel />
+        </div>
+        
+        {/* Timeline Panel - Right */}
+        <div id="timeline-panel" className="flex-1 bg-gray-800" style={{ minWidth: 0 }}>
+          <div className="p-2 border-b border-gray-700">
+            <h2 className="text-lg font-bold text-red-500 text-center">TIMELINE PANEL</h2>
+          </div>
+          <div className="h-56">
+            <Timeline />
           </div>
         </div>
       </div>
